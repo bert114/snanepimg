83364c9895cd47d39768328b1b1be340608e0a59
brt <janalbertpenano14@gmail.com>
2026-04-27 03:42:54 +0800

    replacing the origin from render

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,11 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://lami-si-penans.onrender.com",
+  }),
+);
 app.use(express.json());
 
 app.get("/", (req, res) => {
