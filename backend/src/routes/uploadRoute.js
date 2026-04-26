e3f974d740e0a6831e96e84b4ae2e9cd1d15cc7b
brt <janalbertpenano14@gmail.com>
2026-04-27 04:59:46 +0800

    upload an array of photo

--- backend/src/routes/uploadRoute.js
+++ backend/src/routes/uploadRoute.js
@@ -3,6 +3,6 @@ import { upload } from "../middlewares/upload.js";
 import { uploadController } from "../controllers/userUpload.js";
 const router = express.Router();
 
-router.post("/upload", upload.single("image"), uploadController);
+router.post("/upload", upload.array("images", 100), uploadController);
 
 export default router;
