a27acbb7fb2eb88b934f43184b34039d323b5a83
brt <janalbertpenano14@gmail.com>
2026-04-27 11:21:46 +0800

    fixing path

--- backend/src/services/cloudinaryService.js
+++ backend/src/services/cloudinaryService.js
@@ -1,5 +1,7 @@
+import { bufferToBase64 } from "../utils/imageUtils.js";
+
 export const saveDb = async (req) => {
-  const base64Image = bufferToBase64(imageBuffer);
+  const base64Image = bufferToBase64(req.file.buffer);
 
   const { originalname, mimetype, size, path, filename } = req;
 
