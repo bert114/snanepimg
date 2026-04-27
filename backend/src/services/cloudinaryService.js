0cd7eb731583193ae5fc2cc8bbf7438f2390403a
brt <janalbertpenano14@gmail.com>
2026-04-27 11:25:24 +0800

    remove buffer

--- backend/src/services/cloudinaryService.js
+++ backend/src/services/cloudinaryService.js
@@ -1,8 +1,6 @@
 import { bufferToBase64 } from "../utils/imageUtils.js";
 
 export const saveDb = async (req) => {
-  const base64Image = bufferToBase64(req.file.buffer);
-
   const { originalname, mimetype, size, path, filename } = req;
 
   const data = await Upload.create({
